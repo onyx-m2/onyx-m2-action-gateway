482c0518e9b2dd4a6be2dda9d9275970bb79db5d
John McCalla <johnmccalla@users.noreply.github.com>
2021-04-18 23:59:49 -0400

    feat: check credentials on startup

--- routes/car.js
+++ routes/car.js
@@ -2,85 +2,106 @@ import {
   loginAsync as teslaLogin,
   refreshTokenAsync as teslaRefreshToken,
   windowControlAsync as teslaWindowControl,
-  wakeUpAsync as teslaWakeUp
+  wakeUpAsync as teslaWakeUp,
+  vehiclesAsync as teslaVehicles
 } from 'teslajs'
 import config from '../config.js'
 
 const MILLISECONDS_PER_WEEK = 604800000
 
-const options = {
-  authToken: null,
-  vehicleID: config.tesla.vehicleId
-}
+var authToken = null
 var refreshToken = null
+var vehicleID = config.tesla.vehicleId
+
 
 // once a week we'll force the refreshing of our token (it expires every 45 days)
 setInterval(() => {
-  options.authToken = null
+  authToken = null
 }, MILLISECONDS_PER_WEEK);
 
 export default async function (fastify, opts) {
   fastify.put('/car/vent_windows', ventWindows)
   fastify.put('/car/close_windows', closeWindows)
   fastify.put('/car/wake_up', wakeUp)
+
+  // verify credentials and vehicle configuration
+  await refreshCredentials(fastify.log)
+  const vehicles = await teslaVehicles({ authToken })
+  if (!vehicleID) {
+    fastify.log.warn(`TESLA_VEHICLE_ID is not set, use one of the vehicles below:`)
+    for (const vehicle of vehicles) {
+      const { id, display_name: name, vin } = vehicle
+      fastify.log.info(`Id ${id} is ${name} with vin ${vin}`)
+    }
+  }
+  else {
+    const vehicle = vehicles.find(v => v.id == vehicleID)
+    if (vehicle) {
+      const { display_name: name, vin } = vehicle
+      fastify.log.info(`Selected tesla vehicle is ${name} with vin ${vin}`)
+    }
+    else {
+      fastify.log.error(`No tesla vehicle with id ${vehicleID}`)
+    }
+  }
 }
 
 async function ventWindows(request, reply) {
-  await teslaApi(request, () => teslaWindowControl(options, 'vent'))
+  await teslaApi(request.log, () => teslaWindowControl({ authToken, vehicleID }, 'vent'))
   reply.code(204)
 }
 
 async function closeWindows(request, reply) {
-  await teslaApi(request, () => teslaWindowControl(options, 'close'))
+  await teslaApi(request.log, () => teslaWindowControl({ authToken, vehicleID }, 'close'))
   reply.code(204)
 }
 
 async function wakeUp(request, reply) {
-  await teslaApi(request, () => teslaWakeUp(options))
+  await teslaApi(request.log, () => teslaWakeUp({ authToken, vehicleID }))
   reply.code(204)
 }
 
-async function refreshCredentials(request) {
-  if (options.authToken === null) {
+async function refreshCredentials(log) {
+  if (authToken === null) {
     if (refreshToken !== null) {
       try {
         const { refreshToken: newRefreshToken, authToken: newAuthToken } = await teslaRefreshToken(refreshToken)
         refreshToken = newRefreshToken
-        options.authToken = newAuthToken
-        request.log.info(`Successfully refreshed tesla auth token`)
+        authToken = newAuthToken
+        log.info(`Successfully refreshed tesla auth token`)
       }
       catch (e) {
-        request.log.error(`Error refreshing tesla auth token, will try logging again. ${e}`)
+        log.error(`Error refreshing tesla auth token, will try logging again. ${e}`)
       }
     }
     else {
       try {
         const { username, password } = config.tesla
         const { refreshToken: newRefreshToken, authToken: newAuthToken } = await teslaLogin(username, password)
-        options.authToken = newAuthToken
+        authToken = newAuthToken
         refreshToken = newRefreshToken
-        request.log.info(`Successfully obtained tesla auth token`)
+        log.info(`Successfully obtained tesla auth token`)
       }
       catch (e) {
-        request.log.error(`Error logging into tesla account. ${e}`)
+        log.error(`Error logging into tesla account. ${e}`)
         throw e
       }
     }
   }
 }
 
-async function teslaApi(request, action) {
+async function teslaApi(log, action) {
   try {
-    await refreshCredentials(request)
-    await action()
+    await refreshCredentials(log)
+    return await action()
   }
   catch (e) {
     if (typeof(e) === 'string' && e.includes('408')) {
-      request.log.error(`Timeout during tesla api call, possibly car is asleep.`)
+      log.error(`Timeout during tesla api call, possibly car is asleep.`)
       throw { statusCode: 408, message: 'Car is asleep' }
     }
     else {
-      request.log.error(`Error during tesla api call. ${e}`)
+      log.error(`Error during tesla api call. ${e}`)
       throw { statusCode: 500 }
     }
   }
--- routes/garage.js
+++ routes/garage.js
@@ -18,36 +18,57 @@ setInterval(() => {
 export default async function (fastify, opts) {
   fastify.put('/garage/open_door', openDoor)
   fastify.put('/garage/close_door', closeDoor)
+
+  // verify credentials and device
+  await refreshCredentials(fastify.log)
+  const { devices } = await myQ.getDevices()
+  if (!device) {
+    fastify.log.warn(`MYQ_DEVICE is not set, use one of the devices below:`)
+    for (const info of devices) {
+      const { serial_number: id, name } = info
+      fastify.log.info(`Serial number (device) ${id} is ${name}`)
+    }
+  }
+  else {
+    const info = devices.find(d => d.serial_number == device)
+    if (info) {
+      const { serial_number: id, name } = info
+      fastify.log.info(`Selected garage door is ${name} with serial number ${id}`)
+    }
+    else {
+      fastify.log.error(`No myQ device with id ${device}`)
+    }
+  }
 }
 
 async function openDoor(request, reply) {
-  await myQApi(request, () => myQ.setDoorState(device, OPEN))
+  await myQApi(request.log, () => myQ.setDoorState(device, OPEN))
   reply.code(204)
 }
 
 async function closeDoor(request, reply) {
-  await myQApi(request, () => myQ.setDoorState(device, CLOSE))
+  await myQApi(request.log, () => myQ.setDoorState(device, CLOSE))
   reply.code(204)
 }
 
-async function refreshCredentials(request) {
+async function refreshCredentials(log) {
   if (myQToken === null) {
     try {
       const { username, password } = config.myQ
       const { securityToken } = await myQ.login(username, password)
       myQToken = securityToken
-      request.log.info(`Successfully obtained myQ security token`)
+      log.info(`Successfully obtained myQ security token`)
     }
     catch (e) {
-      request.log.error(`Error logging into myQ account. ${e}`)
+      log.error(`Error logging into myQ account. ${e}`)
       throw e
     }
   }
 }
 
-async function myQApi(request, action) {
+async function myQApi(log, action) {
   try {
-    await refreshCredentials(request)
+    await refreshCredentials(log)
     await action()
   }
   catch (e) {
